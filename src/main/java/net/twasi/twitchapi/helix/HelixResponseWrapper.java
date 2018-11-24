f94a11e0ee458f5f9d98fd6e2d0d8e5ae65857d4
Lars Bärtschi <lars.baertschi@bluewin.ch>
2018-11-24 21:58:20 +0100

    ignore unknown keys in helix response (error object eg)

--- src/main/java/net/twasi/twitchapi/helix/HelixResponseWrapper.java
+++ src/main/java/net/twasi/twitchapi/helix/HelixResponseWrapper.java
@@ -1,7 +1,10 @@
 package net.twasi.twitchapi.helix;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class HelixResponseWrapper<T> {
 
     private List<T> data;
